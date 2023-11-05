9c0a334ae2c2d9e35bc3325ab28009ab3568a9e5
Abulqasim <abulqasim.nazarov@gmail.com>
2023-11-05 20:24:04 +0400

    Create Project by dotnet CLI

---
+++ ServerGuessNumber/Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+Socket socket = new Socket(
+    addressFamily: AddressFamily.InterNetwork,
+    socketType: SocketType.Stream,
+    protocolType: ProtocolType.Tcp);
+
+
+IPAddress address = IPAddress.Parse("192.168.1.7");
+IPEndPoint endPoint = new IPEndPoint(null, 8080);
+socket.Bind();
