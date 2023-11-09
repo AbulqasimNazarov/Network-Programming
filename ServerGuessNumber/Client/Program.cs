9a04f33bbd2ba3a76cddc3dcc94ef2a4cf62f252
Abulqasim <abulqasim.nazarov@gmail.com>
2023-11-09 23:56:40 +0400

    Finish Project

--- ServerGuessNumber/Client/Program.cs
+++ ServerGuessNumber/Client/Program.cs
@@ -1,2 +1,73 @@
-﻿
-Console.WriteLine("Client");
+﻿using System.Net;
+using System.Net.Sockets;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+const string ip = "localhost";
+const int port = 8080;
+
+IPAddress address = IPAddress.Parse(ip);
+IPEndPoint endPoint = new IPEndPoint(address, 8080);
+
+Socket clientSocket = new Socket(
+    addressFamily: AddressFamily.InterNetwork,
+    socketType: SocketType.Stream,
+    protocolType: ProtocolType.Tcp);
+
+//Console.WriteLine("Client");
+await clientSocket.ConnectAsync(endPoint);
+int answerInt = 0;
+var buffer = new byte[1024];
+bool cap = true;
+int i = 0;
+while (true)
+{
+    
+    var size = await clientSocket.ReceiveAsync(buffer);
+
+    int randomNumber = buffer[0];
+    
+    
+    while (cap)
+    {
+        Console.WriteLine($"Correct answer: {randomNumber}");
+        Console.Write("Answer: ");
+        var answerClient = Console.ReadLine();
+
+
+        if (int.TryParse(answerClient, out answerInt))
+        {
+            if (randomNumber == answerInt)
+            {
+                Console.WriteLine("WIN!!!");
+                cap = false;
+                break;
+            }
+            else
+            {
+                i++;
+                if (i >= 3)
+                {
+                    Console.WriteLine("LOSE!");
+                    
+                    cap = false;
+                }
+                break;
+            }
+            
+        }
+        else
+        {
+            Console.WriteLine("Incorrect input!");
+        }
+
+        
+    }
+    
+
+   
+
+
+
+   
+
+}
--- ServerGuessNumber/Server/Program.cs
+++ ServerGuessNumber/Server/Program.cs
@@ -1,12 +1,57 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
-Socket socket = new Socket(
+
+const string ip = "localhost"; 
+const int port = 8080;
+
+IPAddress address = IPAddress.Parse(ip);
+IPEndPoint endPoint = new IPEndPoint(address, 8080);
+
+Socket serverSocket = new Socket(
     addressFamily: AddressFamily.InterNetwork,
     socketType: SocketType.Stream,
     protocolType: ProtocolType.Tcp);
 
 
-IPAddress address = IPAddress.Parse("192.168.1.7");
-IPEndPoint endPoint = new IPEndPoint(null, 8080);
-socket.Bind();
+
+serverSocket.Bind(endPoint);
+serverSocket.Listen(backlog: 5);
+
+Console.WriteLine("Server started!");
+
+
+var buffer = new byte[1024];
+
+
+while (true)
+{
+    Socket clientSocket = await serverSocket.AcceptAsync();
+    Console.WriteLine($"{clientSocket.RemoteEndPoint} connected");
+    
+
+    ThreadPool.QueueUserWorkItem(async (socket) =>
+    {
+        try
+        {
+            int randomNumber = Random.Shared.Next(12, 40);
+            while (true)
+            {
+                await clientSocket.SendAsync(new byte[] { (byte)randomNumber });
+                Thread.Sleep(1000);
+            }
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine($"{socket.RemoteEndPoint} disconnected!");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"System error");
+        }
+
+
+    }, clientSocket, false);
+}
+
